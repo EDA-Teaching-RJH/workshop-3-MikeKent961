ff1929ab05ce3fd6314271af3cd163293ae951ab
MikeKent961 <mk961@kent.ac.uk>
2024-10-11 10:47:08 +0000

    i wanna make part eight a kinda dnd-esque story, but also eight 2 is experiment for something bigger

--- partEight.py
+++ partEight.py
@@ -1,4 +1,31 @@
 import time
+hangover = True
+inventory = [""]
+locations = ["Bush", "Street", "Tavern"]
+currentLocation = "Bush"
+HP = 30
+
+def action(user):
+    if user == "Move" or "move":
+        print("Move to:")
+        for i in locations:
+            if i != currentLocation:
+                print(locations[i])
+        currentLocation = input(": ")
+    
+
+
+
+
 
 input("Stupid story by Me (press enter to continue) ")
+print("You awaken in a bush.")
+print("You sit up and look around to realise you are sitting outside the tavern.")
+print("About a second later, a splitting headache pierces through your thoughts.")
+print("'Ah,' you manage to think, 'this must be because of those 16 beers I was...'")
+print("That's about as far as your thinking goes before your mind starts to throb in pain.")
+print("You should probably find a way to stop being hungover.")
 print("")
+action(input("Move/Look "))
+
+
